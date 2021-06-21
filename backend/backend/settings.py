84b42c9352671e1d34db8e03dc5735a58e4719dd
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-06-21 22:38:13 +0100

    Set up django-cors-headers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "blog",
     "graphene_django",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -122,4 +124,7 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 GRAPHENE = {"SCHEMA": "blog.schema.schema"}
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = "http://localhost:8080"
 
