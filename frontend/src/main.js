229d71492bb446a7138743806071f7a8c3798a60
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-06-21 22:55:41 +0100

    Set up Vue Router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
+import router from '@/router'
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
