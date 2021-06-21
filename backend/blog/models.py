3f2fb3822310ac80b3a0300168cdb89450ff3bc5
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-06-21 21:42:01 +0100

    Create Profile model

---
+++ backend/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.conf import settings
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    website = models.URLField(blank=True)
+    bio = models.CharField(max_length=240, blank=True)
+
+    def __str__(self):
+        return self.user.get_username()
